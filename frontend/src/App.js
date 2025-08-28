f952393dbbe9a44d6e981f5e31734f7cc10a0876
Shi Yang Ng <ngshiyang@gmail.com>
2025-08-28 23:06:47 +0800

    small updates to navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import DashboardPage from './pages/DashboardPage';
 import './App.css';
+// import "@fontsource/montserrat"; 
+// import "@fontsource/montserrat/400.css"; 
+// import "@fontsource/montserrat/400-italic.css"; 
 
 function App() {
   return (
-    <div className="App">
-      <DashboardPage />
+    <div className="my-4 mx-4">
+      <DashboardPage/>
     </div>
   );
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-    return (
-        <header>
-            <h2>Kakak Agent</h2>
-        </header>
-    );
+  return (
+    <header className="flex items-center font-montsera mb-8">
+      <p className="mr-4 font-bold">Kakak Agent</p>
+      <button className="mr-4">Chat</button>
+      <button>Ticket</button>
+      
+    </header>
+  );
 };
 
 export default Header;
