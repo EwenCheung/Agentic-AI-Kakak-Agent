403123ecbe91bbac09f3de2fe2e24a9f2a7ecf9f
Shi Yang Ng <ngshiyang@gmail.com>
2025-09-01 17:13:53 +0800

    addded dashboard and channel linking UI

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,6 @@ const Header = () => {
   return (
     <header className="flex items-center font-montsera mb-8">
       <p className="mr-4 font-bold">Kakak Agent</p>
-      <button className="mr-4">Chat</button>
       <button>Ticket</button>
       
     </header>
--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import Header from '../components/Header';
+import React from "react";
+import Header from "../components/Header";
+import DashBoardIcon from "../assets/dashboard.png";
 
 const DashboardPage = () => {
-    return (
-        <div>
-            <Header />
-            <h1>Dashboard</h1>
+  return (
+    <div>
+      <div className="rounded-lg py-4">
+        <div className="flex items-center mb-2">
+          <h1 className="text-xl mr-2">Dashboard</h1>
+          <img src={DashBoardIcon} className="w-5 h-5" alt="Dashboard Icon" />
         </div>
-    );
+
+        <div className="border border-black px-4 py-2 rounded-lg h-64">
+          <p>Daily Digest</p>
+          <hr class="border-t border-gray-300 mt-2"></hr>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
